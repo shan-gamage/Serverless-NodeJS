8f56efc58e4edc21b464e0ef4a03fe7b9aef2635
Shanaka-Veridate <s.gamage@veridatefinancial.com>
2023-03-12 16:14:32 +0800

    Initiated the serverless node-typscript project

--- cryptonator/index.ts
+++
@@ -1,7 +0,0 @@
-export const handler =  async (event: any) => {
-    console.log(event);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hi Shan you can do this right"})
-    }
-}
